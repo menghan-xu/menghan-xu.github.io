a8a444bd85beb5911452211a3483911ec8c66c57
menghan-xu <mx253@cornell.edu>
2026-08-13 18:42:46 +0000

    Deploying to gh-pages from @ menghan-xu/menghan-xu.github.io@747c7f36a03799618ceb7489fb24922d155a35c2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blogs",
-          title: "blogs",
+  },{id: "nav-posts",
+          title: "posts",
           description: "",
           section: "Navigation",
           handler: () => {
